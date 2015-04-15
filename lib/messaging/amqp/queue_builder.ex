5b7058a30f1f0aa0f2e6a2ddf2da46a569c23e93
Matt Sawka <matt.sawka@perceptivesoftware.com>
2015-04-15 09:26:27 -0500

    add options to queue builder

--- lib/messaging/amqp/queue_builder.ex
+++ lib/messaging/amqp/queue_builder.ex
@@ -25,17 +25,24 @@ defmodule OpenAperture.Messaging.AMQP.QueueBuilder do
 
   The `exchange_id` option defines the exchange id to retrieve  
 
+  The `options` option deinfes additional Queue options
+
   ## Return Values
 
   {:ok, pid} | {:error, reason}
   """
-  @spec build(pid, String.t(), String.t()) :: OpenAperture.Messaging.Queue.t
-	def build(api, queue_name, exchange_id) do
+  @spec build(pid, String.t(), String.t(), List) :: OpenAperture.Messaging.Queue.t
+	def build(api, queue_name, exchange_id, options \\ []) do
+    options_default = [
+      durable: true, 
+      arguments: [{"x-dead-letter-exchange", :longstr, ""},{"x-dead-letter-routing-key", :longstr, "#{queue_name}_error"}]
+    ]
+
 		%Queue{
       name: queue_name, 
       exchange: ExchangeResolver.get(api, exchange_id),
       error_queue: "#{queue_name}_error",
-      options: [durable: true, arguments: [{"x-dead-letter-exchange", :longstr, ""},{"x-dead-letter-routing-key", :longstr, "#{queue_name}_error"}]],
+      options: Keyword.merge(options_default, options),
       binding_options: [routing_key: queue_name]
     }
 	end
